59c074f1a4318b562d0d5249fba4a6d19a4ede11
Bhawna198 <122960027+Bhawna198@users.noreply.github.com>
2025-07-10 20:53:27 +0530

    Fix CountryStateController import case sensitivity

--- routes/countryStateRoute.js
+++ routes/countryStateRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const countryStateController = require('../controllers/CountryStateController');
+const countryStateController = require('../controllers/countryStateController');
 
 router.get('/', countryStateController.getAll);
 router.get('/:id', countryStateController.getById);
