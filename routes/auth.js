0c1e557b72dc94187585f912f16d408bab8ea293
Bhawna198 <122960027+Bhawna198@users.noreply.github.com>
2025-07-10 20:52:28 +0530

    Fix bcrypt module error by using bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authenticateToken = require('../middleware/auth');
 const User = require('../models/User'); // Sequelize User model
